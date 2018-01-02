88abb96155ce08513c10e55acb439790f58b007e
ElenaOrozco <elena.orozcoch@gmail.com>
2018-01-02 13:14:43 -0600

    Ej 1

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -20,7 +20,7 @@ class MessagesController extends Controller
 
     public function create(CreateMessageRequest $request){
 
-    	//dd($request->all());
+    	//dd($request->user());
     	//return 'Created!';
 
     	//Validaciones cortas (para largas crear request)
@@ -33,8 +33,12 @@ class MessagesController extends Controller
     			'message.max' => 'El mensaje no puede superar los 160 caracteres.',
     		]);*/
 
+
+        $user = $request->user();
+
     	//Guarda en la BD	
     	$message = Message::create([
+            'user_id' => $user->id,
     		'content' => $request->input('message'),
     		'image' => 'http://lorempixel.com/600/338?' .mt_rand(0, 1000),
     	]);
