8cf4b142a69f253158bab68089cb70a5f6f7a0b8
ElenaOrozco <elena.orozcoch@gmail.com>
2018-01-09 12:19:03 -0600

    Update ModificacionPregeneradoUsers.php

--- resources/views/teoria/ModificacionPregeneradoUsers.php
+++ resources/views/teoria/ModificacionPregeneradoUsers.php
@@ -2,7 +2,7 @@
 
 /*
 1. Comenzar con la migration
-php artisan migrate make:migration add_username_andavarar_to_users_table --table users
+php artisan make:migration add_username_andavarar_to_users_table --table users
 
 2.Ir a la migracion y sobre escribir los metodos up y down
 
