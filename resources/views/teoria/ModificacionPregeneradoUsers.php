cfad49384f70cbc34efd4b11e51921af165e0ab2
ElenaOrozco <elena.orozcoch@gmail.com>
2018-01-09 07:26:31 -0600

    Modificacion de pregenerado! 2

--- resources/views/teoria/ModificacionPregeneradoUsers.php
+++ resources/views/teoria/ModificacionPregeneradoUsers.php
@@ -6,9 +6,15 @@ php artisan migrate make:migration add_username_andavarar_to_users_table --table
 
 2.Ir a la migracion y sobre escribir los metodos up y down
 
+3. Correr la migracion
+php artisan migrate
+
 */
 //up
  $table->string('username')->unique();
             $table->string('avatar')->nullable();
         });
 //Down
+
+  $table->dropColumn('username');
+            $table->dropColumn('avatar');
